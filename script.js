fcdf420fc38201b3eab476e12073adb382e991c2
diego <diegonegocios21@outlook.com>
2023-05-16 21:15:09 -0300

    Completed

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const lamp = document.getElementById('space_lamp');
+
+lamp.addEventListener("click", () => {
+   lamp.classList.toggle("div_bulb-off")
+   lamp.classList.toggle("div_bulb-on")
+})
+
